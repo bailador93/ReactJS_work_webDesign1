9bb18c6cd5c5f3dd62421b9bc284310003a7c025
Gastón <gebailador_dev@hotmail.com>
2023-11-12 10:55:00 -0300

    First commit

---
+++ src/components/UI/FooterApp.js
@@ -0,0 +1,9 @@
+
+export const FooterApp = () => {
+
+    return <div className="footer_app_content">
+        <div>
+            <span>©2023 Uvi - Designed and Developed by <a href="https://www.linkedin.com/in/bailador93/" target="_blank" rel="noreferrer">Gastón E. Bailador</a></span>
+        </div>
+    </div>;
+}
